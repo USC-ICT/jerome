316d01f5bce0b7028c6f379ace44f322ea921896
leuski-ict <leuski@ict.usc.edu>
2016-04-06 17:42:56 -0700

    first commit

---
+++ jerome.osx Tests/classifier_Tests.mm
@@ -0,0 +1,223 @@
+//
+//  classifier_Tests.m
+//
+//  Created by Anton Leuski on 12/31/14.
+//  Copyright (c) 2014 Anton Leuski & ICT/USC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include <jerome/npc/classifier.hpp>
+#include <jerome/npc/model_cpp.hpp>
+#include <jerome/npc/parsing.hpp>
+#include <jerome/ir/evaluation.hpp>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#include <boost/accumulators/statistics/mean.hpp>
+#pragma clang diagnostic pop
+
+#include <jerome/accumulators/statistics/average.hpp>
+#include <jerome/accumulators/statistics/f_measure.hpp>
+#include <jerome/accumulators/statistics/ranked_list.hpp>
+#include <jerome/accumulators/statistics/best_score_value.hpp>
+#include <jerome/accumulators/statistics/macro_average.hpp>
+#include <jerome/accumulators/statistics/micro_average.hpp>
+
+#include <jerome/accumulators/collectors/collector.hpp>
+#include <jerome/accumulators/collectors/feature_collector.hpp>
+#include <jerome/accumulators/collectors/composite_collector.hpp>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wshorten-64-to-32"
+#include <nlopt.hpp>
+#pragma clang diagnostic pop
+
+#include <jerome/ir/rm/distances_in_answer_space.hpp>
+#include <jerome/ir/rm/distances_in_mixed_space.hpp>
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+@interface ClassifierTests : XCTestCase
+
+@end
+
+using namespace jerome;
+using namespace jerome::npc;
+using namespace jerome::math;
+using namespace jerome::math::parameters;
+using namespace boost::accumulators;
+
+static unsigned int sCount = 0;
+
+static double myvfunc(const std::vector<double> &x, std::vector<double> &grad, void *my_func_data)
+{
+	++sCount;
+	jerome::ir::evaluation::objective_function* func = static_cast<jerome::ir::evaluation::objective_function*>(my_func_data);
+	double value = (*func)(x, grad);
+	std::cout << x << " = " << value << std::endl;
+	std::cout.flush();
+	return value;
+}
+
+struct MyDistanceComputationDelegate : public jerome::ir::DistanceComputationDelegate {
+	WeightMatrix::value_type& operator ()(std::size_t i, std::size_t j) override {
+		return (*x)(i, j);
+	}
+	void setObjectCount(std::size_t count) {
+		x = new SymmetricWeightMatrix(count, count);
+	}
+	SymmetricWeightMatrix* x;
+};
+
+@implementation ClassifierTests
+
+- (void)setUp
+{
+	[super setUp];
+	// Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+	// Put teardown code here. This method is called after the invocation of each test method in the class.
+	[super tearDown];
+}
+
+- (void)test
+{
+// --train-data $(SRCROOT)/../data/twins.plist.csxml --classifier-name "Anyone - The Twins"
+	
+	std::string stateName = "Anyone - The Twins";
+	
+	NSString* baseFolder = [[NSFileManager defaultManager] currentDirectoryPath];
+	NSString* dataFilePath = [[[baseFolder stringByDeletingLastPathComponent]
+														 stringByAppendingPathComponent:@"data"]
+														stringByAppendingPathComponent:@"twins.plist.csxml"];
+	
+	std::string trainDataFile = [dataFilePath fileSystemRepresentation];
+	
+	configure([[[baseFolder stringByAppendingPathComponent:@"jerome"]
+							stringByAppendingPathComponent:@"irregular_verbs.txt"]
+						 fileSystemRepresentation],
+						
+						[[[[[baseFolder stringByDeletingLastPathComponent]
+							 stringByAppendingPathComponent:@"3rd_party"]
+							stringByAppendingPathComponent:@"kstem" ]
+							stringByAppendingPathComponent:@"kstem-lexicon-beta"]
+						 fileSystemRepresentation]
+						);
+	
+	jerome::npc::cpp::ObjectFactory	of;
+	jerome::npc::Collection					collection 	= loadCollection(of, trainDataFile.c_str());
+	std::cout << "loaded training data from " << trainDataFile << std::endl;
+	
+	State						state;
+	XCTAssertTrue(findStateWithName(collection, stateName.c_str(), state), @"Cannot find state %s", stateName.c_str());
+	
+	Data						data	= dataFromState(state, collection.utterance_index());
+	//		Ranker			ranker		= unigramClassifier(collection, vm[oStateName].as<std::string>().c_str());
+	
+	std::pair<Data, Data>		testTrainSplit = data.split(0.05);
+	std::pair<Data, Data>		devTrainSplit = testTrainSplit.second.split(0.5);
+	
+	auto	analyzer	= Unigram::makeUnigram("Unigram", "unigram");
+	
+	auto			ranker	= unigramRanker(state, devTrainSplit.second);
+	
+	typedef jerome::ir::training::TestSet<jerome::ir::HeapIndex, Utterance>	TestSet;
+	
+	TestSet	devSet(devTrainSplit.first, ranker.analyzer()); //UnigramBigram::makeAnalyzer()
+	TestSet	testSet(testTrainSplit.first, ranker.analyzer());
+	
+	typedef	collector<accumulator_set<bool, features<tag::fmeasure>>>						RLCollector;
+	typedef feature_collector<RLCollector::result_type, tag::ranked_list>						RLCollector1;
+	typedef composite_collector<RLCollector, RLCollector1>										RLCollector2;
+	
+	typedef	feature_collector<RLCollector::result_type, tag::average<tag::fmeasure>>		DSCollector;
+	typedef feature_collector<RLCollector2::result_type, tag::best_score_value<DSCollector>>	DSCollector1;
+	
+	//		jerome::ir::evaluation::DataSetEvaluator<Ranker, TestSet, DSCollector1, RLCollector2>	evaluator(ranker, std::move(devSet));
+	//		auto	uttRanker = boost::bind(ranker, boost::bind(analyzer, _1)); // send ranker input through queryAnalzer first
+	jerome::ir::evaluation::TrainingDataSetEvaluator<decltype(ranker), TestSet, DSCollector, RLCollector>	evaluator(ranker, std::move(devSet));
+	
+	std::vector<double>	ignore;
+	
+	typedef std::chrono::high_resolution_clock Clock;
+	typedef std::chrono::duration<double, std::micro> US;
+	auto t0 = Clock::now();
+	
+	double value = evaluator.operator()(ranker.values(), ignore);
+	
+	auto t1 = Clock::now();
+	std::cout << US(t1-t0).count() << " microseconds\n";
+	
+	std::cout << value << std::endl;
+	
+	MyDistanceComputationDelegate	xx;
+	
+			t0 = Clock::now();
+			jerome::ir::rm::DistanceComputationInMixedSpace<decltype(ranker)>::compute(ranker, xx);
+			t1 = Clock::now();
+			std::cout << US(t1-t0).count() << " microseconds\n";
+	
+//			ranker.distances(xx, pp);
+//	
+//			std::cout << (*xx.x) << std::endl;
+	
+	range_vector	ranges = ranker.ranges();
+	
+	std::vector<double>	lb;
+	lb.reserve(ranges.size());
+	std::vector<double>	ub;
+	ub.reserve(ranges.size());
+	
+	for(const RangeDomain& r : ranges) {
+		lb.push_back(r.minimum());
+		ub.push_back(r.maximum());
+	}
+	
+	nlopt::opt lopt(nlopt::LN_BOBYQA, (unsigned int)ranges.size());
+	
+	lopt.set_lower_bounds(lb);
+	lopt.set_upper_bounds(ub);
+	lopt.set_xtol_abs(1e-4);
+	lopt.set_ftol_rel(1e-3);
+	lopt.set_max_objective(myvfunc, &evaluator);
+	
+	
+	nlopt::opt gopt(nlopt::GN_DIRECT_L, (unsigned int)ranges.size());
+	
+	gopt.set_local_optimizer(lopt);
+	gopt.set_lower_bounds(lb);
+	gopt.set_upper_bounds(ub);
+	gopt.set_xtol_abs(1e-4);
+	gopt.set_ftol_rel(1e-4);
+	
+	gopt.set_max_objective(myvfunc, &evaluator);
+	
+	double				maxf;
+	std::vector<double>	x	= ranker.values();
+	x[0] = 0.5; x[1] = 0.5;
+	
+	nlopt::result result = gopt.optimize(x, maxf);
+	
+	t0 = Clock::now();
+	value = evaluator.operator()(x, ignore);
+	t1 = Clock::now();
+	std::cout << US(t1-t0).count() << " microseconds\n";
+	
+	std::cout << result << " " << x << std::endl;
+	
+	std::cout << ::sCount << " calls " << maxf << " " << value << std::endl;
+	
+	lopt.set_ftol_rel(1e-5);
+	result = lopt.optimize(x, maxf);
+}
+
+@end
---
+++ jerome.osx Tests/jerome_osx_Tests.mm
@@ -0,0 +1,302 @@
+//
+//  jerome_osx_Tests.m
+//  jerome.osx Tests
+//
+//  Created by Anton Leuski on 8/2/14.
+//  Copyright (c) 2014 Anton Leuski & ICT/USC. All rights reserved.
+//
+
+#if defined(__Check)
+#error "check"
+#endif
+
+#include <jerome/scripting/js_engine.hpp>
+#include <jerome/npc/model_cpp.hpp>
+
+#import <XCTest/XCTest.h>
+
+@interface ScionTest : XCTestCase
+@property (nonatomic, copy)	NSString*	name;
+@property (nonatomic, copy)	NSString*	scxmlPath;
+@property (nonatomic, copy)	NSString*	jsonPath;
+@property (nonatomic, copy)	NSString*	scionPath;
+@property (nonatomic, copy)	NSString*	jeromeSrcPath;
+@end
+
+@implementation ScionTest
+@synthesize name = _name;
+
++ (XCTestSuite*)defaultTestSuite
+{
+	XCTestSuite*		testSuite		= [[XCTestSuite alloc] initWithName:NSStringFromClass(self)];
+	
+	NSString*			baseFolderPath = [[NSFileManager defaultManager] currentDirectoryPath];
+//	NSString*			baseFolderPath = @"~/Documents/Projects/jerome/jerome";
+//	baseFolderPath = [baseFolderPath stringByStandardizingPath];
+	
+	NSString*			scionFolderPath	= [[[baseFolderPath stringByDeletingLastPathComponent]
+																	 stringByAppendingPathComponent:@"3rd_party"]
+																	 stringByAppendingPathComponent:@"SCION"];
+	NSLog(@"%@", scionFolderPath);
+	
+	NSString*			jeromeSrcFolderPath	= [[baseFolderPath stringByAppendingPathComponent:@"jerome"]
+																		 stringByAppendingPathComponent:@"scripting"];
+	NSLog(@"%@", jeromeSrcFolderPath);
+	
+	NSString*			testsFolderPath	= [[scionFolderPath stringByAppendingPathComponent:@"scxml-test-framework-master"]
+																	 stringByAppendingPathComponent:@"test"];
+	NSLog(@"%@", testsFolderPath);
+	
+	
+	for(NSString* testFolderName in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:testsFolderPath error:nil]) {
+		BOOL		isdir;
+		NSString*	testFolderPath	= [testsFolderPath stringByAppendingPathComponent:testFolderName];
+		if (![[NSFileManager defaultManager] fileExistsAtPath:testFolderPath isDirectory:&isdir] || !isdir) continue;
+		for(NSString* testScriptName in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:testFolderPath error:nil]) {
+			if (![testScriptName hasSuffix:@".scxml"]) continue;
+			NSString*	scxmlPath	= [testFolderPath stringByAppendingPathComponent:testScriptName];
+			NSString*	jsonPath	= [testFolderPath stringByAppendingPathComponent:[[testScriptName stringByDeletingPathExtension] stringByAppendingPathExtension:@"json"]];
+			if (![[NSFileManager defaultManager] fileExistsAtPath:jsonPath]) continue;
+			
+			NSString*	name = [[scxmlPath substringFromIndex:testsFolderPath.length+1] stringByDeletingPathExtension];
+
+			ScionTest*	test = [[ScionTest alloc] initWithSelector:@selector(test)];
+			test.name = name;
+			test.scxmlPath = [[NSURL fileURLWithPath:scxmlPath] absoluteString];
+			test.jsonPath = [[NSURL fileURLWithPath:jsonPath] absoluteString];
+			test.jeromeSrcPath = [[NSURL fileURLWithPath:jeromeSrcFolderPath] absoluteString];
+			test.scionPath = [[NSURL fileURLWithPath:scionFolderPath] absoluteString];
+
+			[testSuite addTest:test];
+		}
+	}
+	
+	return testSuite;
+}
+
+namespace js = jerome::javascript;
+
+- (void)test
+{
+	jerome::scripting::Engine	engine((jerome::npc::Collection()));
+	
+	bool testsAreDone = NO;
+	js::Context&	context(engine.context());
+	
+	engine.performBlock([&] {
+		context.exceptionHandler() = [&](const js::Value& exception, const js::String& inExceptionContext) {
+			testsAreDone = YES;
+			XCTFail(@"JS exception %@ while %@", @((const char*)exception), @(inExceptionContext.c_str()));
+		};
+		
+		context["scionTestsAreDone"] = [&] {
+			testsAreDone = YES;
+		};
+	});
+	
+	NSString* scionMainScriptFilePath = [[self.scionPath stringByAppendingPathComponent:@"dist"]
+																			 stringByAppendingPathComponent:@"scion.js"];
+	NSString* startupScriptFilePath = [self.jeromeSrcPath stringByAppendingPathComponent:@"startup.js"];
+	NSString* scionTestScriptFilePath = [self.jeromeSrcPath stringByAppendingPathComponent:@"scion.tests.js"];
+	
+	engine.evaluateScriptWithFilePath([scionMainScriptFilePath fileSystemRepresentation]);
+	engine.evaluateScriptWithFilePath([startupScriptFilePath fileSystemRepresentation]);
+	engine.evaluateScriptWithFilePath([scionTestScriptFilePath fileSystemRepresentation]);
+	
+	engine.performBlock([&] {
+		
+		engine.postEvent("foo", {{"one", "two"}, {"foo", "bar"} }, "");
+		
+		//		function testOneScionCase(name, scxmlFileName, eventsFileName,
+		//								  failed, errored, passed, log)
+		
+		auto failed = [&] {
+			XCTFail(@"failed: %@", @(jerome::scripting::Engine::argumentsDescription().c_str()));
+			testsAreDone = true;
+		};
+		
+		auto errored = [&] {
+			XCTFail(@"errored: %@", @(jerome::scripting::Engine::argumentsDescription().c_str()));
+			testsAreDone = true;
+		};
+		
+		context["testOneScionCase"]([self.name UTF8String],
+										  [self.scxmlPath UTF8String],
+										  [self.jsonPath UTF8String],
+										  failed,
+										  errored,
+										  [&] {testsAreDone = true;},
+										  context["__scionPlatform"]["log"]
+										  );
+	});
+	
+	while (!testsAreDone) {
+		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.01]];
+	}
+	
+}
+
+@end
+
+@interface jerome_osx_Tests : XCTestCase
+
+@end
+
+@implementation jerome_osx_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+struct A {
+	void f() {}
+	void initialize(JSContextRef ctx, JSObjectRef object);
+	static void hasProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName);
+	static JSValueRef getProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName);
+	static bool setProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, JSValueRef* exception);
+};
+
+void A::initialize(JSContextRef ctx, JSObjectRef object)
+{
+	
+}
+void A::hasProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName)
+{
+	
+}
+JSValueRef A::getProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName)
+{
+	return nullptr;
+}
+bool A::setProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, JSValueRef* exception)
+{
+	return false;
+}
+
+struct B{};
+
+namespace jerome { namespace javascript {
+	template <>
+	struct ClassTraits<A> : public DefaultClassTraits<A> {};
+}}
+
+- (void)testConversionForClassWithTraits
+{
+	jerome::javascript::Context ctx;
+	jerome::javascript::detail::to_valueRef<A>::convert(ctx, A());
+}
+
+- (void)testCallable
+{
+	std::cout << jerome::is_callable<decltype(&A::f)>::value << std::endl;
+	std::cout << jerome::is_callable<void(void)>::value << std::endl;
+	std::cout << jerome::is_callable<void(*)(void)>::value << std::endl;
+	std::cout << jerome::is_callable<void*>::value << std::endl;
+	std::cout << jerome::is_callable<int>::value << std::endl;
+	auto x = [](int x) -> bool { return true; };
+	std::cout << jerome::is_callable<decltype(x)>::value << std::endl;
+}
+
+- (void)testParameterizedClass
+{
+	jerome::javascript::Context	ctx;
+	jerome::javascript::Value		value = ctx.newObject();
+	value.prototype() = ctx.newObject();
+	value["a"] = ctx.newObject();
+	value[0] = ctx.newObject();
+	jerome::javascript::Value		a = value.prototype();
+	a = value["a"];
+	
+	const jerome::javascript::Value	cvalue(value);
+	a = cvalue["a"];
+
+	a = value[0];
+	a = value;
+	a = 5;
+	a = 0;
+	a = 3.14;
+	a = nullptr;
+	a = "hello";
+	value["a"] = "a";
+	value["a"] = nullptr;
+	value["a"] = [](int x) -> bool { return true; };
+	value["a"] = [&](int x) -> bool { return true; };
+	value["a"] = (bool (*)(int))([](int x) -> bool { return true; });
+	value["a"] = (std::function<bool(int)>)([](int x) -> bool { return true; });
+}
+
+//- (void)testExample
+//{
+//	
+//	using namespace jerome;
+//	using namespace jerome::npc;
+//
+//	jerome::npc::cpp::ObjectFactory	of;
+//	jerome::npc::Collection collection;
+//	
+//	NSURL*		srcRootURL	= [NSURL fileURLWithPath:@"/Users/leuski/Documents/Projects/jerome/jerome/jerome/scripting"];
+//	NSURL*		scionRootURL= [NSURL fileURLWithPath:@"/Users/leuski/Documents/Projects/jerome/3rd_party/SCION"];
+//	
+//	NSURL*		scionURL	= [scionRootURL URLByAppendingPathComponent:@"dist/scion.js"];
+//	NSString*	scionStr	= [NSString stringWithContentsOfURL:scionURL encoding:NSUTF8StringEncoding error:nil];
+//	
+//	static jerome::shared_ptr<jerome::scripting::Engine> theEngine(new jerome::scripting::Engine(collection));
+//	theEngine->evaluateScript([scionStr UTF8String], [[scionURL absoluteString] UTF8String]);
+//	
+//	NSURL*		scion1URL	= [srcRootURL URLByAppendingPathComponent:@"startup.js"];
+//	NSString*	scion1Str	= [NSString stringWithContentsOfURL:scion1URL encoding:NSUTF8StringEncoding error:nil];
+//	
+//	theEngine->evaluateScript([scion1Str UTF8String], [[scion1URL absoluteString] UTF8String]);
+//	
+//	NSURL*		scion2URL	= [srcRootURL URLByAppendingPathComponent:@"scion.tests.js"];
+//	NSString*	scion2Str	= [NSString stringWithContentsOfURL:scion2URL encoding:NSUTF8StringEncoding error:nil];
+//	
+//	theEngine->evaluateScript([scion2Str UTF8String], [[scion2URL absoluteString] UTF8String]);
+//	
+//	//	NSURL*		testURL	= [NSURL URLWithString:@"file:///Users/leuski/Documents/Projects/mVH/3rd_party/SCION/test/surface-test.js"];
+//	//	NSString*	testStr	= [NSString stringWithContentsOfURL:testURL encoding:NSUTF8StringEncoding error:nil];
+//	//	theEngine.context().evaluateScript([testStr UTF8String], [[testURL absoluteString] UTF8String]);
+//	
+//	NSString*			testsFolderPath	= [[scionRootURL URLByAppendingPathComponent:@"scxml-test-framework-master/test"] path];
+//	NSMutableString*	code	= [NSMutableString stringWithString:@"test(["];
+//	int count = 0;
+//	for(NSString* testFolderName in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:testsFolderPath error:nil]) {
+//		BOOL		isdir;
+//		NSString*	testFolderPath	= [testsFolderPath stringByAppendingPathComponent:testFolderName];
+//		if (![[NSFileManager defaultManager] fileExistsAtPath:testFolderPath isDirectory:&isdir] || !isdir) continue;
+//		for(NSString* testScriptName in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:testFolderPath error:nil]) {
+//			if (![testScriptName hasSuffix:@".scxml"]) continue;
+//			NSString*	scxmlPath	= [testFolderPath stringByAppendingPathComponent:testScriptName];
+//			NSString*	jsonPath	= [testFolderPath stringByAppendingPathComponent:[[testScriptName stringByDeletingPathExtension] stringByAppendingPathExtension:@"json"]];
+//			if (![[NSFileManager defaultManager] fileExistsAtPath:jsonPath]) continue;
+//			if (count) {
+//				[code appendString:@","];
+//			}
+//			[code appendFormat:@"[\"%@\", \"%@\"]", [[NSURL fileURLWithPath:scxmlPath] absoluteString], [[NSURL fileURLWithPath:jsonPath] absoluteString]]; count++;
+//			//			NSLog(@"%@ and %@", [scxmlPath lastPathComponent], [jsonPath lastPathComponent]);
+//		}
+//	}
+//	[code appendString:@"]);"];
+//	
+//	String	script([code UTF8String]);
+//	theEngine->evaluateScript(script);
+////	jerome::async::sharedInstance().strand().dispatch(boost::bind(&jerome::scripting::Engine::evaluateScript, theEngine, script, String(), 0, true));
+//	
+//	std::cout << "test started" << std::endl;
+//	while (!testIsDone)
+//		usleep(1000);
+//	std::cout << "test done" << std::endl;
+//
+////    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+//}
+//
+
+@end
