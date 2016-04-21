ebcb49ef415c913571cd5482592b7e7aaf745473
Anton Leuski <anton@leuski.net>
2016-04-21 15:30:57 -0700

    copy jerome public headers to the build folder

---
+++ scripts/copy_headers.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#  copy_headers.sh
+#  jerome
+#
+#  Created by Anton Leuski on 4/20/16.
+#  Copyright © 2016 Anton Leuski & ICT/USC. All rights reserved.
+
+## copy public header files preserving directory structure
+
+cd "${SRCROOT}/jerome"
+rsync  -Cavm --filter="+ */" \
+  --filter="- *.private.h" \
+  --filter="+ *.hpp" \
+  --filter="+ *.h" \
+  --filter="- *"  \
+. "${BUILT_PRODUCTS_DIR}/include/${PRODUCT_NAME}"
