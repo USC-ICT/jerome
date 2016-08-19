2e550a4c5ebfed9b0cbb1d055996ca5c82512a2a
Anton Leuski <anton@leuski.net>
2016-08-19 10:09:12 -0700

    fixing constructor ambiguity.

--- jerome/ir/collection.hpp
+++ jerome/ir/collection.hpp
@@ -134,7 +134,7 @@ namespace jerome { namespace ir {
 			
 			explicit Field()
 			: mTotalTermCount(0)
-      , mDocumentLengths((int)0)
+      , mDocumentLengths((size_type)0)
       {
       }
 			
