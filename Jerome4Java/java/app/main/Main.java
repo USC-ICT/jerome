6f6274a1e458ba9d507a3beea70ce09f25f14454
Anton Leuski <leuski@ict.usc.edu>
2019-09-15 16:23:27 -0700

    Removing window in Java testing app

--- Jerome4Java/java/app/main/Main.java
+++ Jerome4Java/java/app/main/Main.java
@@ -8,20 +8,71 @@
 
 package main;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
+import jerome.Jerome;
+import jerome.Utterance;
 
-public class Main extends JFrame {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  private static Object LOCK = new Object();
+
   public static void main(String[] inArgs) {
     System.out.println(System.getProperty("java.library.path"));
     System.out.println(new File(".").getAbsolutePath());
-    new Main(inArgs).setVisible(true);
+
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    Jerome mJerome = new Jerome();
+
+    mJerome.loadModel(new File(inArgs[0]), new File(inArgs[1]), inArgs[2],
+        (e) -> {
+          if (e != null) {
+            System.err.println("error in loader: " + e);
+            System.exit(1);
+          }
+        });
+
+    try {
+      while (true) {
+
+        System.out.print("Enter your question: ");
+        String question = input.readLine().trim();
+        if (question == "" || question.toLowerCase() == "q") {
+          break;
+        }
+
+        mJerome.respondTo(question, new Jerome.ResponseCallback() {
+          @Override
+          public void didSelectUtterance(Utterance inUtterance,
+              Jerome.Completion inCompletion)
+          {
+            System.out.println(inUtterance.valueForKey("text"));
+            inCompletion.succeeded();
+          }
+
+          @Override
+          public void didFinishProcessing() {
+            System.out.println("NLU done");
+            synchronized (LOCK) {
+              LOCK.notify();
+            }
+          }
+        });
+
+        synchronized (LOCK) {
+          LOCK.wait();
+        }
+      }
+
+    } catch (Throwable inE) {
+      inE.printStackTrace();
+    }
+
   }
 
-  public Main(String[] inArgs) throws HeadlessException {
-    setSize(400, 300);
-    setContentPane(new MainWindow(inArgs).getContentPane());
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
+
 }
